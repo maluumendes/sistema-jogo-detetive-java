995b64aeeb7e65afe9dd6c561b030446e958b5c6
Julia Soares Catharino <102225501+juliascatharino@users.noreply.github.com>
2026-06-12 14:47:27 -0300

    Update CasoPersistencia.java

--- src/Persistence/CasoPersistencia.java
+++ src/Persistence/CasoPersistencia.java
@@ -1,29 +1,22 @@
 package Persistence;
 
 import Model.Caso;
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class CasoPersistencia {
-
     private final String arquivo = "casos.txt";
 
-    public void salvar(ArrayList<Caso> casos){
-
-        try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter(arquivo));
-
+    public void salvar(ArrayList<Caso> casos) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(arquivo))) {
             for (Caso c : casos) {
-                bw.write(c.getId() + ";" + c.getTitulo() + ";" + c.getDescricao() + ";" + c.getLocalCrime() + ";" + c.getDataCrime() + ";" + c.getStatus() );
+                bw.write(c.getId() + ";" + c.getTitulo() + ";" + c.getDescricao() + ";" + c.getLocalCrime() + ";" + c.getDataCrime() + ";" + c.getStatus());
                 bw.newLine();
             }
-            bw.close();
-        } catch (IOException e){
-            System.out.println("Erro ao salvar arquivo.");
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar arquivo de casos: " + e.getMessage());
         }
     }
-
 }
