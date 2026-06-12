2ebad8449d5332f1451ee6f7ce305118e6b81b6b
Julia Soares Catharino <102225501+juliascatharino@users.noreply.github.com>
2026-06-12 14:44:24 -0300

    Update Investigador.java

--- src/Model/Investigador.java
+++ src/Model/Investigador.java
@@ -1,26 +1,48 @@
 package Model;
 
-public class Investigador extends Pessoa{
-
+public class Investigador extends Pessoa {
+    private int id;
+    private String cargo;
+    private String departamento;
     private int limiteCasosSimultaneos;
     private int casosAtivosAtuais;
     private String metodoInvestigacao;
     private String itemDeInvestigacaoFavorito;
 
-    public Investigador(String nome, String ocupacao, int idade, int limiteCasosSimultaneos, int casosAtivosAtuais, String metodoInvestigacao, String itemDeInvestigacaoFavorito ) {
-        super(nome, ocupacao, idade);
-        this.limiteCasosSimultaneos = limiteCasosSimultaneos;
+    public Investigador(int id, String nome, String cargo, String departamento) {
+        super(nome, cargo, 35); 
+        this.id = id;
+        this.cargo = cargo;
+        this.departamento = departamento;
+        this.limiteCasosSimultaneos = 3;
         this.casosAtivosAtuais = 0;
-        this.metodoInvestigacao = metodoInvestigacao;
-        this.itemDeInvestigacaoFavorito = itemDeInvestigacaoFavorito;
+        this.metodoInvestigacao = "Dedução Lógica";
+        this.itemDeInvestigacaoFavorito = "Lupa";
     }
 
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getCargo() { return cargo; }
+    public void setCargo(String cargo) { this.cargo = cargo; }
+
+    public String getDepartamento() { return departamento; }
+    public void setDepartamento(String departamento) { this.departamento = departamento; }
+
+    public int getLimiteCasosSimultaneos() { return limiteCasosSimultaneos; }
+    public void setLimiteCasosSimultaneos(int limite) { this.limiteCasosSimultaneos = limite; }
+
+    public int getCasosAtivosAtuais() { return casosAtivosAtuais; }
+    public void setCasosAtivosAtuais(int ativos) { this.casosAtivosAtuais = ativos; }
+
+    public String getMetodoInvestigacao() { return metodoInvestigacao; }
+    public void setMetodoInvestigacao(String metodo) { this.metodoInvestigacao = metodo; }
+
+    public String getItemDeInvestigacaoFavorito() { return itemDeInvestigacaoFavorito; }
+    public void setItemDeInvestigacaoFavorito(String item) { this.itemDeInvestigacaoFavorito = item; }
+
     public boolean podeAssumirNovoCaso() {
-        if (this.casosAtivosAtuais < this.limiteCasosSimultaneos) {
-            return true;
-        } else {
-            return false;
-        }
+        return this.casosAtivosAtuais < this.limiteCasosSimultaneos;
     }
 
     public void alocarAoCaso() {
@@ -37,46 +59,12 @@ public class Investigador extends Pessoa{
 
     @Override
     public String gerarResumoDetalhado() {
-        return "Investigador{" +
-                "Nome: '" + getNome() + '\'' +
-                ", Idade: " + getIdade() +
-                ", Ocupação: '" + getOcupacao() + '\'' +
-                ", Limite de Casos: " + limiteCasosSimultaneos +
-                ", Casos Ativos: " + casosAtivosAtuais +
-                ", Método: '" + metodoInvestigacao + '\'' +
-                ", Item Favorito: '" + itemDeInvestigacaoFavorito + '\'' +
-                '}';
+        return "Investigador [ID: " + id + " | Nome: " + getNome() + " | Cargo: " + cargo + 
+               " | Dept: " + departamento + " | Casos Ativos: " + casosAtivosAtuais + "/" + limiteCasosSimultaneos + "]";
     }
 
-    public String getItemDeInvestigacaoFavorito() {
-        return itemDeInvestigacaoFavorito;
-    }
-
-    public void setItemDeInvestigacaoFavorito(String itemDeInvestigacaoFavorito) {
-        this.itemDeInvestigacaoFavorito = itemDeInvestigacaoFavorito;
-    }
-
-    public String getMetodoInvestigacao() {
-        return metodoInvestigacao;
-    }
-
-    public void setMetodoInvestigacao(String metodoInvestigacao) {
-        this.metodoInvestigacao = metodoInvestigacao;
-    }
-
-    public int getCasosAtivosAtuais() {
-        return casosAtivosAtuais;
-    }
-
-    public void setCasosAtivosAtuais(int casosAtivosAtuais) {
-        this.casosAtivosAtuais = casosAtivosAtuais;
-    }
-
-    public int getLimiteCasosSimultaneos() {
-        return limiteCasosSimultaneos;
-    }
-
-    public void setLimiteCasosSimultaneos(int limiteCasosSimultaneos) {
-        this.limiteCasosSimultaneos = limiteCasosSimultaneos;
+    @Override
+    public String toString() {
+        return getNome() + " (ID: " + id + ")";
     }
 }
