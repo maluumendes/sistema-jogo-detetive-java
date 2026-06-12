0f3bf013e7edf63d28ae225774c15c67d9f0abbf
Julia Soares Catharino <102225501+juliascatharino@users.noreply.github.com>
2026-06-12 14:44:52 -0300

    Update Suspeito.java

--- src/Model/Suspeito.java
+++ src/Model/Suspeito.java
@@ -1,50 +1,33 @@
 package Model;
 
-public class Suspeito extends Pessoa{
+public class Suspeito extends Pessoa {
+    private int id;
     private String alibi;
     private String relacaoComVitima;
-    private boolean possuiAntecendentes;
+    private boolean possuiAntecedentes;
 
-    public Suspeito(String nome, String ocupacao, int idade, String alibi, String relacaoComVitima, boolean possuiAntecendentes) {
+    public Suspeito(int id, String nome, String ocupacao, int idade, String alibi, String relacaoComVitima, boolean possuiAntecedentes) {
         super(nome, ocupacao, idade);
+        this.id = id;
         this.alibi = alibi;
         this.relacaoComVitima = relacaoComVitima;
-        this.possuiAntecendentes = possuiAntecendentes;
+        this.possuiAntecedentes = possuiAntecedentes;
     }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getAlibi() { return alibi; }
+    public void setAlibi(String alibi) { this.alibi = alibi; }
+
+    public String getRelacaoComVitima() { return relacaoComVitima; }
+    public void setRelacaoComVitima(String relacao) { this.relacaoComVitima = relacao; }
+
+    public boolean isPossuiAntecedentes() { return possuiAntecedentes; }
+    public void setPossuiAntecedentes(boolean antecedentes) { this.possuiAntecedentes = antecedentes; }
 
     @Override
     public String gerarResumoDetalhado() {
-        return "Supeito{" +
-                "Nome: '" + getNome() + '\'' +
-                ", Idade: " + getIdade() +
-                ", Ocupação: '" + getOcupacao() + '\'' +
-                ", Alibi: " + alibi + '\'' +
-                ", Relação com a vítima: " + relacaoComVitima +
-                ", Possui antecedentes?: '" + possuiAntecendentes + '\'' +
-                '}';
-    }
-
-    public String getAlibi() {
-        return alibi;
-    }
-
-    public void setAlibi(String alibi) {
-        this.alibi = alibi;
-    }
-
-    public String getRelacaoComVitima() {
-        return relacaoComVitima;
-    }
-
-    public void setRelacaoComVitima(String relacaoComVitima) {
-        this.relacaoComVitima = relacaoComVitima;
-    }
-
-    public boolean getPossuiAntecendentes() {
-        return possuiAntecendentes;
-    }
-
-    public void setPossuiAntecendentes(boolean possuiAntecendentes) {
-        this.possuiAntecendentes = possuiAntecendentes;
+        return "Suspeito [ID: " + id + " | Nome: " + getNome() + " | Álibi: " + alibi + " | Antecedentes: " + possuiAntecedentes + "]";
     }
 }
