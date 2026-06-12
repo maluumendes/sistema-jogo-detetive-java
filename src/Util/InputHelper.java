60c77cd82ce1468fd304404129f56965a90488d0
Julia Soares Catharino <102225501+juliascatharino@users.noreply.github.com>
2026-06-12 14:52:52 -0300

    Update InputHelper.java

--- src/Util/InputHelper.java
+++ src/Util/InputHelper.java
@@ -3,17 +3,22 @@ package Util;
 import java.util.Scanner;
 
 public class InputHelper {
+    private static Scanner scanner = new Scanner(System.in);
 
-    static Scanner scan = new Scanner(System.in);
-
-    public static String lerTexto(String texto){
-        System.out.println(texto);
-        return scan.nextLine();
+    public static int lerNumInt(String prompt) {
+        System.out.print(prompt);
+        while (!scanner.hasNextInt()) {
+            System.out.println("Por favor, insira um número inteiro válido.");
+            System.out.print(prompt);
+            scanner.next();
+        }
+        int num = scanner.nextInt();
+        scanner.nextLine(); // Limpa buffer
+        return num;
     }
 
-    public static int lerNumInt(String texto){
-        System.out.println(texto);
-        return Integer.parseInt(scan.nextLine());
+    public static String lerTexto(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine();
     }
-
 }
