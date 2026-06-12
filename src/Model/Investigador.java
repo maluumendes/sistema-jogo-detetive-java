7dc22e23115a20a69923157e91c42bdbbb8f1ad0
Julia Soares Catharino <102225501+juliascatharino@users.noreply.github.com>
2026-06-12 14:59:07 -0300

    Update Investigador.java

--- src/Model/Investigador.java
+++ src/Model/Investigador.java
@@ -2,32 +2,22 @@ package Model;
 
 public class Investigador extends Pessoa {
     private int id;
-    private String cargo;
-    private String departamento;
     private int limiteCasosSimultaneos;
     private int casosAtivosAtuais;
     private String metodoInvestigacao;
     private String itemDeInvestigacaoFavorito;
 
-    public Investigador(int id, String nome, String cargo, String departamento) {
-        super(nome, cargo, 35); 
+    public Investigador(int id, String nome, String ocupacao, int idade, int limiteCasosSimultaneos, int casosAtivosAtuais, String metodoInvestigacao, String itemDeInvestigacaoFavorito) {
+        super(nome, ocupacao, idade);
         this.id = id;
-        this.cargo = cargo;
-        this.departamento = departamento;
-        this.limiteCasosSimultaneos = 3;
-        this.casosAtivosAtuais = 0;
-        this.metodoInvestigacao = "Dedução Lógica";
-        this.itemDeInvestigacaoFavorito = "Lupa";
+        this.limiteCasosSimultaneos = limiteCasosSimultaneos;
+        this.casosAtivosAtuais = casosAtivosAtuais;
+        this.metodoInvestigacao = metodoInvestigacao;
+        this.itemDeInvestigacaoFavorito = itemDeInvestigacaoFavorito;
     }
 
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
-
-    public String getCargo() { return cargo; }
-    public void setCargo(String cargo) { this.cargo = cargo; }
-
-    public String getDepartamento() { return departamento; }
-    public void setDepartamento(String departamento) { this.departamento = departamento; }
 
     public int getLimiteCasosSimultaneos() { return limiteCasosSimultaneos; }
     public void setLimiteCasosSimultaneos(int limite) { this.limiteCasosSimultaneos = limite; }
@@ -59,8 +49,9 @@ public class Investigador extends Pessoa {
 
     @Override
     public String gerarResumoDetalhado() {
-        return "Investigador [ID: " + id + " | Nome: " + getNome() + " | Cargo: " + cargo + 
-               " | Dept: " + departamento + " | Casos Ativos: " + casosAtivosAtuais + "/" + limiteCasosSimultaneos + "]";
+        return "Investigador [ID: " + id + " | Nome: " + getNome() + " | Posto: " + getOcupacao() + 
+               " | Idade: " + getIdade() + " | Casos Ativos: " + casosAtivosAtuais + "/" + limiteCasosSimultaneos + 
+               " | Método: " + metodoInvestigacao + " | Item Favorito: " + itemDeInvestigacaoFavorito + "]";
     }
 
     @Override
